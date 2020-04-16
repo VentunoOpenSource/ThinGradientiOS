4138236878d40a40d5a67fc28b3a2a71140c5cad
Kathiresan <kathir@ventunotech.com>
2020-04-16 12:00:25 +0530

    first commit

--- GradientViewPOC/ViewController.swift
+++ GradientViewPOC/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var mGradientLayer:CAGradientLayer?    
+    @IBOutlet weak var mGradientHld: UIView!
+    @IBOutlet weak var mLeftGradientHld: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createRightGradientLayer()
+        createLeftGradientLayer()
+       
+    }
+    
+    func createRightGradientLayer() {
+        if let mGradientHld = mGradientHld {
+            mGradientHld.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            mGradientLayer = CAGradientLayer()
+            mGradientLayer?.frame = mGradientHld.bounds
+            mGradientLayer?.colors = [UIColor.blue.cgColor, UIColor.black.cgColor]
+            mGradientLayer?.startPoint = CGPoint(x: 1, y: -0.2);
+            mGradientLayer?.endPoint = CGPoint(x: 0.0, y: 0.0);
+            if let mGradientLayer = mGradientLayer {
+                mGradientHld.layer.insertSublayer(mGradientLayer, at: 0)
+            }
+        }
+    }
+    
+    func createLeftGradientLayer() {
+        if let mGradientHld = mLeftGradientHld {
+            mGradientHld.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            mGradientLayer = CAGradientLayer()
+            mGradientLayer?.frame = mGradientHld.bounds
+            mGradientLayer?.colors = [UIColor.red.cgColor, UIColor.black.cgColor]
+            mGradientLayer?.startPoint = CGPoint(x: 0.0, y: 0.0);
+            mGradientLayer?.endPoint = CGPoint(x: 0.75, y: 0.2);
+            if let mGradientLayer = mGradientLayer {
+                mGradientHld.layer.insertSublayer(mGradientLayer, at: 0)
+            }
+        }
     }
 
-
 }
+
+
+
+
 
