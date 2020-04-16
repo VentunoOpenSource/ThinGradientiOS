e8eb50a00794ea97a850f71564d70ebbad8c937c
Vijayakumar Moorthy <vijay@ventunotech.com>
2020-04-16 10:34:06 +0530

    Initial Commit

---
+++ GradientViewPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientViewPOC
+//
+//  Created by Ventuno Technologies on 16/04/20.
+//  Copyright © 2020 Ventuno Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
